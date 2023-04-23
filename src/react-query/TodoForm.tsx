88cc04634e84c2e094e8e8e7abf94582f60ec27b
KishorPokharel <kishorpokharel7@gmail.com>
2023-04-23 11:47:23 +0545

    mutating data

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,10 +1,45 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useRef } from 'react';
+import { Todo } from './hooks/useTodos';
+import axios from 'axios';
 
 const TodoForm = () => {
+  const queryClient = useQueryClient();
+  const addTodo = useMutation({
+    mutationFn: (todo: Todo) => {
+      return axios
+        .post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
+        .then((res) => res.data);
+    },
+    onSuccess: (savedTodo, newTodo) => {
+      // approach 1: invalidate
+      // queryClient.invalidateQueries({
+      //   queryKey: ['todos'],
+      // });
+
+      // approach 2: update data in cache
+      queryClient.setQueryData<Todo[]>(['todos'], (todos) => [
+        savedTodo,
+        ...(todos || []),
+      ]);
+    },
+  });
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="row mb-3">
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current && ref.current.value) {
+          addTodo.mutate({
+            id: 0,
+            title: ref.current?.value,
+            completed: false,
+            userId: 1,
+          });
+        }
+      }}
+      className="row mb-3">
       <div className="col">
         <input ref={ref} type="text" className="form-control" />
       </div>
