c964cbeeb26d3239b5afa62a50e3a02bb10f206b
KishorPokharel <kishorpokharel7@gmail.com>
2023-04-30 14:42:26 +0545

    custom provider

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,17 @@ import AuthContext from './state-management/contexts/authContext';
 import TasksContext from './state-management/contexts/tasksContext';
 import authReducer from './state-management/reducers/authReducer';
 import tasksReducer from './state-management/reducers/tasksReducer';
+import AuthProvider from './state-management/AuthProvider';
 
 function App() {
   const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
-  const [user, authDispatch] = useReducer(authReducer, '');
   return (
-    <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+    <AuthProvider>
       <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
         <NavBar />
         <HomePage />
       </TasksContext.Provider>
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 }
 
