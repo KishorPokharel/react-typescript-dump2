ed8f1dcdc57004157b371b7455e070edeaa88064
KishorPokharel <kishorpokharel7@gmail.com>
2023-04-30 14:34:45 +0545

    more contexts

--- src/state-management/TaskList.tsx
+++ src/state-management/TaskList.tsx
@@ -1,12 +1,14 @@
-import { useContext, useReducer, useState } from 'react';
-import tasksReducer from './reducers/tasksReducer';
+import { useContext } from 'react';
 import TasksContext from './contexts/tasksContext';
+import AuthContext from './contexts/authContext';
 
 const TaskList = () => {
   const { tasks, dispatch } = useContext(TasksContext);
+  const { user } = useContext(AuthContext);
 
   return (
     <>
+      {user && <p>Welcome {user}</p>}
       <button
         onClick={() =>
           dispatch({
