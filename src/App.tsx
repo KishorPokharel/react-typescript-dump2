aaba7fec5a1637a20cbe020840d6e989f8d3c898
KishorPokharel <kishorpokharel7@gmail.com>
2023-04-30 14:44:19 +0545

    context hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useReducer } from 'react';
 import './App.css';
+import AuthProvider from './state-management/AuthProvider';
 import HomePage from './state-management/HomePage';
 import NavBar from './state-management/NavBar';
-import AuthContext from './state-management/contexts/authContext';
 import TasksContext from './state-management/contexts/tasksContext';
-import authReducer from './state-management/reducers/authReducer';
 import tasksReducer from './state-management/reducers/tasksReducer';
-import AuthProvider from './state-management/AuthProvider';
 
 function App() {
   const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
