bdef5835073b96e7fb4d4ce60319510a66587252
KishorPokharel <kishorpokharel7@gmail.com>
2023-04-30 14:53:58 +0545

    custom task provider and hook

---
+++ src/state-management/hooks/useTasks.ts
@@ -0,0 +1,8 @@
+import { useContext } from 'react';
+import TasksContext from '../contexts/tasksContext';
+
+const useTasks = () => {
+  return useContext(TasksContext);
+};
+
+export default useTasks;
